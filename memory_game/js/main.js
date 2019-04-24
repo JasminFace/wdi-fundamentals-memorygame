3b631d42fd331c50d71473f296df6d061501c30e
Jasmin Saromo <jasminsaromo@gmail.com>
2019-04-23 21:47:49 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,13 @@
-console.log("Up and running!");
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push (cardOne);
+cardsInPlay.push (cardTwo);
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
