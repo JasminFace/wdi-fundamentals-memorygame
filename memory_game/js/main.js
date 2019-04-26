78314d6c9cb1e622b01192e92eddd3a35f70989d
Jasmin Saromo <jasminsaromo@gmail.com>
2019-04-26 17:47:08 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,17 +24,17 @@ const cardsInPlay = [];
 
 function checkForMatch(){
 if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	alert("You found a match!");
 } else {
-	console.log("Sorry, try again.");
+	alert("Sorry, try again.");
 }
 };
 
 function flipCard(cardId){
 console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push (cards[cardId].rank);
 console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
-cardsInPlay.push (cards[cardId.rank]);
 };
 
 flipCard(0);
